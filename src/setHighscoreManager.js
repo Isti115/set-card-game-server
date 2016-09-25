a5f15255a0c1f33675aa2c5757b8ce251163133e
István Donkó <istvan.donko+github@gmail.com>
2016-09-25 18:10:52 +0200

    Actually receiving and sending scores

--- src/setHighscoreManager.js
+++ src/setHighscoreManager.js
@@ -4,6 +4,8 @@ class SetHighscoreManager {
   constructor () {
     this.dbConnected = this.dbConnected.bind(this)
     this.clientRequested = this.clientRequested.bind(this)
+    this.clientMessaged = this.clientMessaged.bind(this)
+    this.clientClosed = this.clientClosed.bind(this)
 
     MongoClient.connect('mongodb://localhost/set', this.dbConnected)
   }
@@ -16,6 +18,8 @@ class SetHighscoreManager {
   clientRequested (ws) {
     console.log('requested')
 
+    this.sendScores(ws)
+
     ws.on('message', (msg) => {
       this.clientMessaged(ws, msg)
     })
@@ -26,17 +30,49 @@ class SetHighscoreManager {
   }
 
   clientMessaged (ws, msg) {
-    console.log('messaged')
-
+    console.log(`messaged at: ${new Date()}`)
     console.log(msg)
 
-    this.scores.find({}).toArray((err, data) => {
-      ws.send(JSON.stringify(data))
-    })
+    const message = JSON.parse(msg)
+
+    if (message.type === 'score') {
+      this.scores.insert({
+        name: message.data.name,
+        score: message.data.score,
+        date: new Date()
+      })
+    }
+
+    this.sendScores(ws)
   }
 
   clientClosed (ws) {
     console.log('closed')
+  }
+
+  sendScores (ws) {
+    this.scores.find({}, { name: 1, score: 1, _id: 0 })
+      .sort({score: -1}).limit(10).toArray((err, data) => {
+      ws.send(JSON.stringify({
+        type: 'globalScores',
+        data: data
+      }))
+    })
+
+    // const date = new Date()
+    // const dayBegin = new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T00:00:00+0200`)
+    // const dayEnd = new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T24:00:00+0200`)
+    //
+    // this.scores.find({ date: { $gt: dayBegin, $lt: dayEnd } }, { name: 1, score: 1, _id: 0 })
+    //   .sort({score: -1}).limit(10).toArray((err, data) => {
+    //   ws.send(JSON.stringify({
+    //     type: 'dailyScores',
+    //     data: data
+    //   }))
+    // })
+
+  // db.scores.group({key: {name: 1}, initial: {max: 0},reduce: function(curr, result) {if(result.max < curr.score){result.max = curr.score}}})
+  // db.scores.aggregate([{$group: {_id: '$name', maxScore: {$max: '$score'}}}])
   }
 }
 
